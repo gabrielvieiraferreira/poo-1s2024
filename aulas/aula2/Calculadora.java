03e971da2300afeee768da8b93181dbd85fc4070
SaintArthurG <cazooklol@gmail.com>
2024-03-20 00:09:38 -0300

    Aula 2

---
+++ aulas/aula2/Calculadora.java
@@ -0,0 +1,23 @@
+package aulas.aula2;
+
+public class Calculadora {
+    Double nmbA;
+    Double nmbB;
+
+    Double somar() {
+        return nmbA + nmbB;
+    }
+
+    Double subtrair() {
+        return nmbA - nmbB;
+    }
+
+    Double multiplicar() {
+        return nmbA * nmbB;
+    }
+
+    Double dividir() {
+        return nmbA / nmbB;
+    }
+}
+
