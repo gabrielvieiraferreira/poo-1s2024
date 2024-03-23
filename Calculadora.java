5f9731f40e5933e753116cc1ffe166834cb1d040
gabrielvieiraferreira <101935758+gabrielvieiraferreira@users.noreply.github.com>
2024-03-23 18:26:01 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,27 @@
+package aulas.atividade1;
+
+public class Calculadora {
+    Double nmbA;
+    Double nmbB;
+
+    Double somar() {
+        return nmbA + nmbB;
+    }
+
+    Double subtrair() {
+        return nmbA - nmbB;
+    }
+
+    Double multiplicar() {
+        return nmbA * nmbB;
+    }
+
+    Double dividir() {
+        return nmbA / nmbB;
+    }
+
+    Boolean verificarNmrPrimo(int nmb){
+        return nmb % 2 == 1;
+    }
+}
+
